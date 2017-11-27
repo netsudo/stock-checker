c77f61144a159a8ee577c0dffbca2b2d190f3863
netsudo <netsudo@users.noreply.github.com>
2017-11-27 00:01:16 -0800

    Made fileLines not depend on checking for EOF for more reliability

--- file.cpp
+++ file.cpp
@@ -5,8 +5,7 @@ std::vector<std::string> File::fileLines(std::string filename) {
 	std::string temp;
 
 	std::ifstream infile(filename.c_str());
-	while ( !infile.eof() ) {
-		std::getline(infile, temp);
+	while ( std::getline(infile, temp) ) {
 		file.push_back(temp);
 	}
 	infile.close();
